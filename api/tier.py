d627ff520ba4ca3a0e2da2c1e28547929ec90c37
grzybear <107876605+grzybear@users.noreply.github.com>
2024-01-10 17:22:55 +0100

    tier vehicles

--- api/tier.py
+++ api/tier.py
@@ -1,4 +1,7 @@
 import requests
+from vehicle import *
+
+default_radius = 1370
 
 class Tier:
     def __init__(self):
@@ -10,6 +13,23 @@ class Tier:
             "Host": "platform.tier-services.io",
         }
 
-    def get_scooters(self, lat, lng, radius):
-        response = requests.get(self.url + "&lat=" + str(lat) + "&lng=" + str(lng) + "&radius=" + str(radius), headers=self.headers)
+    def get_scooters(self, lat, lon, radius):
+        response = requests.get(self.url + "&lat=" + str(lat) + "&lng=" + str(lon) + "&radius=" + str(radius), headers=self.headers)
         return response.json()
+    
+    def get_vehicles(self, init_lat, init_lon):
+        json_obj = self.get_scooters(init_lat, init_lon, default_radius)
+        all_vehicles = []
+        data = json_obj.get("data", {})
+        for row in data:
+            if row.get("type", "") == "vehicle":
+                vehicle_data = row.get("attributes", {})
+                lat = row.get("lat", 0)
+                lon = row.get("lon", 0)
+                charge = row.get("batteryLevel", 0)
+                range = row.get("currentRangeMeters", 0)
+
+                vehicle = Vehicle(vehicle_type='scooter', charge=charge, lat=lat, lon=lon, brand='bolt', range=range)
+                all_vehicles.append(vehicle)
+
+        return all_vehicles
