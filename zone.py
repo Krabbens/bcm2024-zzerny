1c7533bd1d2648e58eea0076fac9a7ca11116678
grzybear <107876605+grzybear@users.noreply.github.com>
2024-01-10 23:51:38 +0100

    zone brand added

--- zone.py
+++ zone.py
@@ -1,11 +1,11 @@
 class Zone:
 
-    def __init__(self, geometry, type, lat, lon):
+    def __init__(self, geometry, type, lat, lon, brand):
         self.geometry = geometry
         self.type = type
+        self.brand = brand
         self.location = (lat, lon)
 
     
     def __str__(self):
-        return f"Zone(type={self.type}, location={self.location})"
-
+        return f"Zone(type={self.type}, location={self.location}, brand={self.brand})"
